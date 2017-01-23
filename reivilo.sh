460b6b5b385aec28f9dc5f90cb50c860d16aa374
halavus <halavus@gmail.com>
2017-01-23 23:07:03 +0100

    first commit

---
+++ reivilo.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+if [ $age_olivier -eq not_that_freaking_old ]
+	mv ~/work /tmp/
+	mv $worries ~/.Trash-1000/
+	mv /family/* /heart/
+	echo '$success'>> /future/README.md
+	for i in $( ls /happiness/ ); do
+		touch ~/even/more/happ.iness
+	done
+
+	echo "Joyeux anniversaire mon cher Olivier ! Comme tu vois j'ai du temps à perdre. Mais ça m'amuse."
+	echo "J'ai même appris ce soir grâce à ce script à quoi servait la commande touch. Gros noob va!"
+	echo "Je ne suis pas sûr que le code fonctionne sur toutes les plateformes, mais on dira que c'est l'intention qui compte..."
+	echo "Grâce à toi et ton conseil d'installer Debian, j'ai enfin un pc qui fonctionne et t'en suis très reconnaissant."
+
+fi
